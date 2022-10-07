5d300a5480e0f18ad52418c72c1917e1b21249c7
lfc2136 Cremers <lfc2136@columbia.edu>
2022-10-07 01:18:29 +0000

    final try to get hreoku working?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
     end
 
     if session[:ratings] and not params[:ratings]
-      @ratings_to_show = session[:ratings].keys
+      @ratings_to_show = session[:full_ratings].keys #session[:ratings].keys
       params[:ratings] = session[:full_ratings]
     end
 
